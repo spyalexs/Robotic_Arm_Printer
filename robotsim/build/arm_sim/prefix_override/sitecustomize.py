e53f0460aa381efb258e7f6845744b7ea78eab2b
markcfong561 <markcfong561@gmail.com>
2023-04-11 23:44:07 -0400

    Started robot sim stuff

---
+++ robotsim/build/arm_sim/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/markc/Robotic_Arm_Printer/robotsim/install/arm_sim'
---
+++ robotsim/src/arm_sim/arm_sim/arm.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from arm_sim.')
+
+
+if __name__ == '__main__':
+    main()
