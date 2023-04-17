bfdf2e8f49a280773a305a5240a302f7b91dfd08
Mark Fong <markcfong561@gmail.com>
2023-04-17 01:01:21 -0400

    started xacro and traj following

--- robotsim/src/arm_sim/setup.py
+++ robotsim/src/arm_sim/setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from glob import glob
+
+import os
 
 package_name = 'arm_sim'
 
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
