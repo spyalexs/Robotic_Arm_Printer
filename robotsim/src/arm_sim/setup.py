73798dc0645b04a8b23f6ca9a1863d63e8cc3cc6
markcfong561 <markcfong561@gmail.com>
2023-04-18 17:36:22 -0400

    added .gitignore and rviz stuff

--- robotsim/src/arm_sim/setup.py
+++ robotsim/src/arm_sim/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'arm_sim'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('description/urdf/*')),
+        ('share/' + package_name, glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
