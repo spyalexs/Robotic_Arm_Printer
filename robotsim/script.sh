5c7e3a88c88b1741e84f250295b869a36d2833c3
Mark Fong <markcfong561@gmail.com>
2023-04-21 02:49:55 -0400

    WE HAVE AN ARM AND I CAN MOVE IT

--- robotsim/script.sh
+++ robotsim/script.sh
@@ -1,35 +1,40 @@
 gz topic -t "/model/printerArm/joint_trajectory" -m gz.msgs.JointTrajectory -p '
     joint_names: "joint1"
     joint_names: "joint2"
+    joint_names: "joint3"
     points {
       positions: -0.7854
       positions: 1.5708
-      time_from_start {
-        sec: 0
-        nsec: 250000000
-      }
-    }
-    points {
-      positions: -1.5708
-      positions: 0
-      time_from_start {
-        sec: 0
-        nsec: 500000000
-      }
-    }
-    points {
-      positions: -1.5708
-      positions: -1.5708
-      time_from_start {
-        sec: 0
-        nsec: 750000000
-      }
-    }
-    points {
-      positions: 0
       positions: 0
       time_from_start {
         sec: 1
+        nsec: 0000000
+      }
+    }
+    points {
+      positions: -1.5708
+      positions: 0
+      positions: 1.5708
+      time_from_start {
+        sec: 2
+        nsec: 000000
+      }
+    }
+    points {
+      positions: -1.5708
+      positions: -1.5708
+      positions: 0
+      time_from_start {
+        sec: 3
+        nsec: 0000000
+      }
+    }
+    points {
+      positions: 0
+      positions: 0
+      positions: 0
+      time_from_start {
+        sec: 4
         nsec: 0
       }
     }'
