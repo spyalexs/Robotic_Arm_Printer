a87789a47c64305610a936fca5b55b6a714ce093
Mark Fong <markcfong561@gmail.com>
2023-04-23 22:30:43 -0400

    Arm moves correctly now

--- robotsim/src/arm_sim/arm_sim/publish_pos.py
+++ robotsim/src/arm_sim/arm_sim/publish_pos.py
@@ -8,6 +8,7 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 import os
 import time
+import math
 
 class posePublisher(Node):
     def __init__(self):
@@ -15,38 +16,21 @@ class posePublisher(Node):
         self.posPub = self.create_publisher(JointTrajectory, '/model/printerArm/joint_trajectory', qos_profile_system_default)
         self.startTime = 0
         time.sleep(1)
-        # self.startGzBridge()
-        self.sendGoal(self.HCodeToJointPoints())
+        self.followPoints()
 
-    def startGzBridge(self):
-        topic = '/model/printerArm/joint_trajectory'
-        gz_type = 'gz.msgs.JointTrajectory'
-        ros_type = 'trajectory_msgs/msg/JointTrajectory'
-        self.get_logger().info(f'Running \'ros2 run ros_gz_bridge parameter_bridge {topic}@{ros_type}]{gz_type}\'')
-        os.system(f'ros2 run ros_gz_bridge parameter_bridge {topic}@{ros_type}]{gz_type}')
-
-    def HCodeToJointPoints(self):
+    def followPoints(self):
         points = []
         f = open(os.path.join(get_package_share_directory('arm_sim'), 'Test_Part_1.hcode'))
-        curNs = 0
-        curS = 0
-        for line in f:
-            jointPos = JointTrajectoryPoint()
-            jointPos.positions = [float(i) for i in line[0:line.index('#')].split()]
-            jointPos.time_from_start.sec = curS
-            jointPos.time_from_start.nanosec = curNs
-            points.append(jointPos)
-            if curNs + 100000000 >= 1e9:
-                curS += 1
-                curNs = 0
-            else:
-                curNs += 100000000
-        return points
-
-    def sendGoal(self, points):
-        self.get_logger().info(str(len(points)))
         out_msg = JointTrajectory()
         out_msg.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+        for line in f:
+            jointPos = JointTrajectoryPoint()
+            jointPos.positions = [float(i) % (2 * math.pi) for i in line[0:line.index('#')].split()]
+            timeToPos = float(line[line.index('#time#') + 6:])
+            jointPos.time_from_start.sec = int(timeToPos)
+            jointPos.time_from_start.nanosec = int(100000000 * (timeToPos % 1))
+            points.append(jointPos)
+        self.get_logger().info("Sending points")
         out_msg.points = points
         self.posPub.publish(out_msg)
 
